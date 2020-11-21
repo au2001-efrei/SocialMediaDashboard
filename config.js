bbe19a4d5e38fb09cfff2416f6cd1916af3c465a
Aurélien <au2001sfg@gmail.com>
2020-11-21 17:37:16 +0100

    Add drop config option

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 export default {
 	port: 3000,
 	secret: '6#98VELCY++5wXu7py%gn5p^pkerM?a%#mY-YTacZP_F5R9t_F2Fj@3zvUvExQu3',
+
 	database: {
 		user: 'aurelien',
 		host: 'localhost',
 		password: 'V7jMuBGm',
 		database: 'WebProject'
 	},
+	drop: false,
 
 	googleClientSecret: "h5nn_VN9KVGZ4zwyCBgGgxWY"
 }
--- server/database.js
+++ server/database.js
@@ -5,6 +5,8 @@ import config from "../config.js"
 const database = new pg.Client(config.database)
 database.connect()
 
+if (config.drop) database.query("DROP TABLE IF EXISTS users")
+
 database.query("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, email VARCHAR NOT NULL, password VARCHAR NOT NULL)")
 
 export default database
