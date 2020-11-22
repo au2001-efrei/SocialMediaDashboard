e9c1f89ef2693cbcbe3b8f0152a21698a20a6827
Aurélien <au2001sfg@gmail.com>
2020-11-22 23:20:48 +0100

    Remove database credentials

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ export default {
 	secret: '6#98VELCY++5wXu7py%gn5p^pkerM?a%#mY-YTacZP_F5R9t_F2Fj@3zvUvExQu3',
 
 	database: {
-		user: 'aurelien',
+		user: 'xxx',
 		host: 'localhost',
-		password: 'V7jMuBGm',
-		database: 'WebProject'
+		password: 'xxx',
+		database: 'xxx'
 	},
 	drop: false,
 
