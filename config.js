e9944cdad70a60bd9e75b538d50ef8e12e0a6dc1
Aurélien <6292584+au2001@users.noreply.github.com>
2021-07-04 13:50:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
 	port: 3000,
-	secret: '6#98VELCY++5wXu7py%gn5p^pkerM?a%#mY-YTacZP_F5R9t_F2Fj@3zvUvExQu3',
+	secret: 'xxx',
 
 	database: {
 		user: 'xxx',
@@ -10,15 +10,15 @@ export default {
 	},
 	drop: false,
 
-	youtubeClientID: "137670576518-b2lnmmfd86v57meudgjo93t2r1255g6j.apps.googleusercontent.com",
-	youtubeClientSecret: "h5nn_VN9KVGZ4zwyCBgGgxWY",
+	youtubeClientID: "xxx",
+	youtubeClientSecret: "xxx",
 
-	twitterConsumerKey: "D88VuUrkLrWzcxgatH0gyXVL2",
-	twitterConsumerSecret: "5ql8nYUJZtdXgQxHwe2KGHjXwhJdoa5PdMNzGkNgvxBEGBO1M4",
+	twitterConsumerKey: "xxx",
+	twitterConsumerSecret: "xxx",
 
-	instagramClientID: "212262967005108",
-	instagramClientSecret: "ebee8f4135ab3d673ec728d4ddc22fd7",
+	instagramClientID: "xxx",
+	instagramClientSecret: "xxx",
 
-	redditConsumerKey: "8qkFELu9xrNwng",
-	redditConsumerSecret: "J3yAhdujso_Nm78YLJvQ3nh5QC8Btw"
+	redditConsumerKey: "xxx",
+	redditConsumerSecret: "xxx"
 }
